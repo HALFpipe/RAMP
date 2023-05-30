4365596b6b255cd5f7a1ae024eaabd6e36734050
Lea Waller <lea.waller@charite.de>
2023-05-30 12:03:14 +0200

    Fix `libStatGen` build

--- recipes/raremetal/build.sh
+++ recipes/raremetal/build.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 
+# Ensure that hard-coded executables exist.
+mkdir -p $(pwd)/bin
+prefix="${BUILD_PREFIX}/bin/${BUILD}-"
+for tool in GCC GXX AR; do
+    executable=$(realpath ${!tool})
+    tool_name=${executable#"${prefix}"}
+    ln -s ${executable} $(pwd)/bin/${tool_name}
+done
+export PATH="$(pwd)/bin:$PATH"
+
+# Use conda's compiler flags.
 cget init --verbose \
-    --cflags "-fopenmp -g" \
-    --cxxflags "-fopenmp -g"
+    --cflags "${CFLAGS} -fopenmp -g" \
+    --cxxflags "${CXXFLAGS} -fopenmp -g"
+
+# Use conda's zlib.
 cget ignore --verbose zlib
 
-# avoid race condition by monkey patching cpu_count to disable
-# parallel builds within `cget`
+# Avoid race condition by monkey patching cpu_count to disable
+# parallel builds within `cget`.
 python - <<EOF
 import multiprocessing
 multiprocessing.cpu_count = lambda: 1
@@ -18,13 +31,15 @@ for package_build in prefix.from_file("requirements.txt"):
     prefix.install(package_build)
 EOF
 
+# Actually build `raremetal`.
 mkdir build
 pushd build
 
 cmake \
-    -DCMAKE_BUILD_TYPE="Release" \
+    -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_TOOLCHAIN_FILE="../cget/cget/cget.cmake" \
-    -DBUILD_TESTS="1" \
+    -DBUILD_TESTS:BOOL=ON \
+    -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON \
     ..
 
 make
