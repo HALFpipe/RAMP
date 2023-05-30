05575a5ddabf3e58df99909cf833ec9ab043657e
Lea Waller <lea.waller@charite.de>
2023-05-30 12:03:14 +0200

    Fix `libStatGen` build

--- recipes/dosage-convertor/build.sh
+++ recipes/dosage-convertor/build.sh
@@ -1,19 +1,30 @@
 #!/bin/bash
 
-# disable security features that are crashing the build
+# Ensure that hard-coded executables exist.
+mkdir -p $(pwd)/bin
+prefix="${BUILD_PREFIX}/bin/${BUILD}-"
+for tool in CC GXX AR; do
+    executable=$(realpath ${!tool})
+    tool_name=${executable#"${prefix}"}
+    ln -s ${executable} $(pwd)/bin/${tool_name}
+done
+export PATH="$(pwd)/bin:$PATH"
+
+# Disable security features that are crashing the build.
 export CPPFLAGS=$(
     echo "$CPPFLAGS" | sed \
-    -e 's/-D_FORTIFY_SOURCE=[^ ]\+//g'
+        -e 's/-D_FORTIFY_SOURCE=[^ ]\+//g'
 )
 export CPPFLAGS="${CPPFLAGS} -U_FORTIFY_SOURCE"
 cget init --verbose \
     --cflags "-U_FORTIFY_SOURCE" \
     --cxxflags "-U_FORTIFY_SOURCE"
 
+# Use conda's zlib.
 cget ignore --verbose zlib
 
-# avoid race condition by monkey patching cpu_count to disable
-# parallel builds within `cget`
+# Avoid race condition by monkey patching cpu_count to disable
+# parallel builds within `cget`.
 python - <<EOF
 import multiprocessing
 multiprocessing.cpu_count = lambda: 1
@@ -25,9 +36,9 @@ for package_build in prefix.from_file("requirements.txt"):
     prefix.install(package_build)
 EOF
 
+# Actually build the project.
 mkdir build
 pushd build
-
 cmake \
     -DCMAKE_BUILD_TYPE="Release" \
     -DCMAKE_TOOLCHAIN_FILE="../cget/cget/cget.cmake" \
