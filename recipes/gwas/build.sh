96b32e7d6da19073e30d54b336684ae3cd66cee4
Lea Waller <lea.waller@charite.de>
2023-06-12 12:20:43 +0200

    Fix python package build
    - No need to manually specify sysroot anymore

--- recipes/gwas/build.sh
+++ recipes/gwas/build.sh
@@ -2,14 +2,10 @@
 
 set -x
 
-CFLAGS="-isystem ${CONDA_BUILD_SYSROOT}/usr/include "${CFLAGS}
-LDFLAGS="-isystem ${CONDA_BUILD_SYSROOT}/usr/include "${LDFLAGS}
-CPPFLAGS="-isystem ${CONDA_BUILD_SYSROOT}/usr/include "${CPPFLAGS}
-
-CFLAGS=$(echo "${CFLAGS}" | sed "s/-O2/-O3/g")
-CPPFLAGS=$(echo "${CPPFLAGS}" | sed "s/-O2/-O3/g")
-CXXFLAGS=$(echo "${CXXFLAGS}" | sed "s/-O2/-O3/g")
+CFLAGS=${CFLAGS//-O2/-O3}
+CPPFLAGS=${CPPFLAGS//-O2/-O3}
+CXXFLAGS=${CXXFLAGS//-O2/-O3}
 
 export CPPFLAGS CFLAGS CXXFLAGS LDFLAGS
 
-python -m pip install --no-deps --ignore-installed "./src/gwas"
+python -m pip install --verbose --no-deps --ignore-installed "./src/gwas"
