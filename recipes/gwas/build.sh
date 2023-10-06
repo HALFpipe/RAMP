8192c3780e7fd7712c87cd15f0a73209cb70ceff
Lea Waller <lea.waller@charite.de>
2023-10-06 13:53:49 +0200

    Fix package build by adding rust compile time dependencies

--- recipes/gwas/build.sh
+++ recipes/gwas/build.sh
@@ -4,8 +4,10 @@ set -x
 
 CFLAGS=${CFLAGS//-O2/-O3}
 CPPFLAGS=${CPPFLAGS//-O2/-O3}
-CXXFLAGS=${CXXFLAGS//-O2/-O3}
+CXXFLAGS=${CXXFLAGS//-O2/-O3}\
 
-export CPPFLAGS CFLAGS CXXFLAGS LDFLAGS
+LD_LIBRARY_PATH=${PREFIX}/lib
+
+export CPPFLAGS CFLAGS CXXFLAGS LDFLAGS LD_LIBRARY_PATH
 
 python -m pip install --verbose --no-deps --ignore-installed "./src/gwas"
