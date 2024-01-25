0dc18760eebfc2b865c0a641ca2e806e82f526d3
Lea Waller <lea.waller@charite.de>
2024-01-25 16:22:46 +0100

    Update flake

--- flake.nix
+++ flake.nix
@@ -1,9 +1,12 @@
 {
   description = "A basic flake with a shell";
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs?ref=pull/259068/head";
     flake-utils.url = "github:numtide/flake-utils";
-    rust-overlay.url = "github:oxalica/rust-overlay";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = { self, nixpkgs, flake-utils, rust-overlay }:
@@ -16,59 +19,41 @@
         };
         compressionPackages = with pkgs; [ bcftools bzip2 htslib p7zip zstd ];
 
-        blosc2 =
-          pkgs.python311Packages.blosc2.overridePythonAttrs (old: rec {
-            pname = "blosc2";
-            version = "2.2.7";
-            src = pkgs.fetchPypi {
-              inherit version pname;
-              hash = "sha256-e22AVEbFYLJgA9H5B+e9No7dgtIygz6fO2F+LIgQjV4=";
-            };
-          });
-        setuptools-rust =
-          pkgs.python311Packages.setuptools-rust.overridePythonAttrs (old: rec {
-            pname = "setuptools-rust";
-            version = "1.7.0";
-            format = "pyproject";
-            src = pkgs.fetchPypi {
-              inherit version pname;
-              hash = "sha256-xxAJmZSCNaOK5+VV/hmapmwlPcOEsSX12FRzv4Hq46M=";
-            };
-          });
-        pythonPackages = with pkgs.python311Packages; [
-          blosc2
-          cython_3
-          matplotlib
-          more-itertools
-          msgpack
-          mypy
-          ndindex
-          networkx
-          numpy
-          pandas
-          pip
-          psutil
-          pytest
-          pytest-benchmark
-          pyyaml
-          scipy
-          seaborn
-          setuptools-rust
-          setuptools-scm
-          threadpoolctl
-          torch
-          tqdm
-          types-pyyaml
-          zstandard
-        ];
+        python = (pkgs.python311.withPackages (py:
+          with py; [
+            blosc2
+            cython_3
+            matplotlib
+            more-itertools
+            msgpack
+            mypy
+            ndindex
+            networkx
+            numpy
+            pandas
+            pip
+            psutil
+            pytest
+            pytest-benchmark
+            pyyaml
+            scipy
+            seaborn
+            setuptools-rust
+            setuptools-scm
+            threadpoolctl
+            torch-bin
+            tqdm
+            types-pyyaml
+            zstandard
+          ]));
         rust = pkgs.rust-bin.beta.latest.default.override {
           extensions = [ "rust-src" ];
         };
       in {
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs;
-            [ mkl plink-ng python311 rust rust-analyzer-unwrapped ]
-            ++ compressionPackages ++ pythonPackages;
+            [ mkl plink-ng python rust rust-analyzer-unwrapped ]
+            ++ compressionPackages;
           RUST_SRC_PATH = "${rust}/lib/rustlib/src/rust/library";
         };
       });
