0967487256122e60315eb3b7a45548c053a27209
Lea Waller <lea.waller@charite.de>
2024-02-23 15:18:52 +0100

    Remove deprecated option in pytorch 2.1

--- src/gwas/src/gwas/__init__.py
+++ src/gwas/src/gwas/__init__.py
@@ -15,7 +15,6 @@ finally:
 os.environ["BLOSC_TRACE"] = "1"
 # os.environ["MKL_VERBOSE"] = "1"
 
-torch.set_default_tensor_type(torch.DoubleTensor)
 torch.set_default_dtype(torch.float64)
 del torch
 del os
