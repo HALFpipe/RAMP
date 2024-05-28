bbbdb57e9a22efccb1fe505d7a72d6c8185b533d
dominikgoeller <22domig@gmail.com>
2024-05-28 12:00:58 +0200

    check categorical to false

--- src/gwas/tests/test_vcf.py
+++ src/gwas/tests/test_vcf.py
@@ -128,7 +128,10 @@ def test_read(benchmark, vcf_path: Path, numpy_read_result: ReadResult, engine: 
 
     # assert np.all(numpy_read_result.variants == read_result.variants)
     assert assert_frame_equal(
-        numpy_read_result.variants, read_result.variants, check_dtype=False
+        numpy_read_result.variants,
+        read_result.variants,
+        check_dtype=False,
+        check_categorical=False,
     )
     assert np.allclose(numpy_read_result.dosages, read_result.dosages)
 
