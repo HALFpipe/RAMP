05862610226c97d3d5a0b31717bc2e8126a0719d
Lea Waller <lea.waller@charite.de>
2024-07-31 16:01:56 +0200

    Add command to convert existing blosc2 score files

---
+++ src/gwas/src/gwas/convert/cli.py
@@ -0,0 +1,49 @@
+import logging
+import multiprocessing as mp
+import os
+import sys
+from argparse import ArgumentParser, Namespace
+from typing import Literal
+
+
+def parse_arguments(argv: list[str]) -> Namespace:
+    """Parses command-line arguments"""
+    argument_parser = ArgumentParser(description="Generate Manhattan & QQ Plots")
+
+    argument_parser.add_argument("path")
+
+    argument_parser.add_argument("--debug", action="store_true", default=False)
+    argument_parser.add_argument(
+        "--log-level", choices=logging.getLevelNamesMapping().keys(), default="INFO"
+    )
+    argument_parser.add_argument("--num-threads", type=int, default=mp.cpu_count())
+
+    return argument_parser.parse_args(argv)
+
+
+def main() -> None:
+    run(sys.argv[1:])
+
+
+def run(argv: list[str], error_action: Literal["raise", "ignore"] = "ignore") -> None:
+    arguments = parse_arguments(argv)
+
+    from gwas.log import logger, setup_logging
+
+    setup_logging(level=arguments.log_level)
+
+    os.environ["NUMEXPR_MAX_THREADS"] = str(arguments.num_threads)
+    os.environ["NUMEXPR_NUM_THREADS"] = str(arguments.num_threads)
+
+    try:
+        from .command import convert
+
+        convert(arguments)
+    except Exception as e:
+        logger.exception("Exception: %s", e, exc_info=True)
+        if arguments.debug:
+            import pdb
+
+            pdb.post_mortem()
+        if error_action == "raise":
+            raise e
---
+++ src/gwas/src/gwas/convert/command.py
@@ -0,0 +1,76 @@
+import pickle
+from argparse import Namespace
+from pathlib import Path
+
+import blosc2
+import numpy as np
+from tqdm.auto import tqdm
+
+from ..compression.arr.base import FileArray, default_compression_method
+from ..compression.pipe import CompressedBytesReader
+from ..log import logger
+
+suffix_to_convert = ".score.b2array"
+
+
+def axis_metadata_path(path: Path) -> Path:
+    return path.parent / f"{path.stem}.axis-metadata.pkl.zst"
+
+
+def convert(arguments: Namespace) -> None:
+    num_threads = arguments.num_threads
+
+    path = Path(arguments.path)
+    if path.is_file():
+        paths = [path]
+    else:
+        paths = list(path.rglob(f"*{suffix_to_convert}"))
+
+    for path in tqdm(paths, unit="files"):
+        if path.is_file():
+            convert_file(path, num_threads)
+
+
+def convert_file(path: Path, num_threads: int) -> None:
+    if path.name.endswith(suffix_to_convert):
+        array = blosc2.open(
+            urlpath=str(path),
+            cparams=dict(nthreads=num_threads),
+            dparams=dict(nthreads=num_threads),
+        )
+        try:
+            vlmeta = array.schunk.vlmeta
+            axis_metadata_bytes = vlmeta.get_vlmeta("axis_metadata")
+            row_metadata, column_metadata = pickle.loads(axis_metadata_bytes)
+        except KeyError:
+            if axis_metadata_path(path).is_file():
+                with CompressedBytesReader(axis_metadata_path(path)) as file_handle:
+                    row_metadata, column_metadata = pickle.load(file_handle)
+            else:
+                row_metadata, column_metadata = None, None
+
+        row_chunk_size, _ = array.chunks
+        row_count, column_count = array.shape
+        stat_file_array_path = path.with_suffix(".txt.zst")
+        if stat_file_array_path.is_file():
+            logger.warning(
+                f"Skipping {path} because {stat_file_array_path} already exists"
+            )
+            return
+        stat_file_array = FileArray.create(
+            stat_file_array_path,
+            (row_count, column_count),
+            np.float64,
+            compression_method=default_compression_method,
+            num_threads=num_threads,
+        )
+        stat_file_array.set_axis_metadata(0, row_metadata)
+        stat_file_array.set_axis_metadata(1, column_metadata)
+
+        with stat_file_array:
+            for row_start in tqdm(
+                range(0, row_count, row_chunk_size), unit="chunks", leave=False
+            ):
+                row_end = min(row_start + row_chunk_size, row_count)
+                row_chunk = array[row_start:row_end, :]
+                stat_file_array[row_start:row_end, :] = row_chunk
