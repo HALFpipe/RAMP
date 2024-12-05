7031af176d3f50de146b05326d89363ceda6b3ac
Lea Waller <lea.waller@charite.de>
2024-12-05 06:37:11 +0100

    Support double_iid output in stratify

--- src/gwas/src/gwas/stratify/base.py
+++ src/gwas/src/gwas/stratify/base.py
@@ -8,6 +8,8 @@ class SampleID(NamedTuple):
     def to_str(self, method: str = "both_with_underscore") -> str:
         if method == "both_with_underscore":
             return f"{self.fid}_{self.iid}"
+        if method == "double_iid":
+            return f"{self.iid}_{self.iid}"
         elif method == "iid":
             return self.iid
         elif method == "fid":
